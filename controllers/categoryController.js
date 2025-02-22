4f77377f5fcf73a11c39dc4d03a69dfe1004c59a
Vindya Sathsarani <123439572+vindyasathsarani@users.noreply.github.com>
2025-02-22 19:36:55 +0530

    Add routes to get all categories and get category by name

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -29,3 +29,65 @@ export function createCategory(req, res) {
       });
     });
 }
+
+export function deleteCategory(req, res) {
+  if (req.user == null) {
+    res.status(401).json({
+      message: "Unauthorized",
+    });
+    return;
+  }
+  if (req.user.type != "admin") {
+    res.status(403).json({
+      message: "Forbidden",
+    });
+    return;
+  }
+  const name = req.params.name;
+  Category.findOneAndDelete({ name: name })
+    .then(() => {
+      res.json({
+        message: "Category deleted successfully",
+      });
+    })
+    .catch(() => {
+      res.json({
+        message: "Category deletion failed",
+      });
+    });
+}
+
+export function getCategory(req, res) {
+  Category.find()
+    .then((result) => {
+      res.json({
+        categories: result,
+      });
+    })
+    .catch(() => {
+      res.json({
+        message: "Failed to get categories",
+      });
+    });
+}
+
+export function getCategoryByName(req, res) {
+  const name = req.params.name;
+  Category.findOne({ name: name })
+    .then((result) => {
+      if (result == null) {
+        res.json({
+          message: "Category not found",
+        });
+      } else {
+        res.json({
+          category: result,
+        });
+      }
+    })
+    .catch(() => {
+      res.json({
+        message: "Failed to get category",
+      });
+    });
+}
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { createCategory } from "../controllers/categoryController.js";
+import { createCategory, deleteCategory, getCategory, getCategoryByName } from "../controllers/categoryController.js";
 
 const categoryRouter = express.Router();
 
 categoryRouter.post("/", createCategory);
+categoryRouter.delete("/:name", deleteCategory)
+categoryRouter.get("/:name", getCategoryByName)
+categoryRouter.get("/", getCategory)
 
 export default categoryRouter;
